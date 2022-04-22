2a77980d77f8b14a8ca6becb4017d7e6c41a42ab
billydean <billy.goehring@gmail.com>
2022-04-22 10:24:10 -0700

    endpoint skeletons done. next include tasks foreign key and booleans

--- api/task/model.js
+++ api/task/model.js
@@ -2,5 +2,22 @@
 // gotta access that database somehow
 const db = require('../../data/dbConfig');
 
+function getTasks() {
+    return db('tasks');
+}
+function getTaskById(task_id) {
+    return db('tasks')
+        .where('task_id', task_id)
+        .first();
+}
+
+function postTask(task) {
+   return db('tasks').insert(task)
+    .then(([task_id]) => {
+        return db('tasks')
+            .where('task_id', task_id)
+            .first()
+    })
+}
 //export the module
-module.exports = {}
+module.exports = {getTasks,getTaskById,postTask}
--- api/task/router.js
+++ api/task/router.js
@@ -3,5 +3,38 @@
 const router = require('express').Router();
 const Task = require('./model');
 
+
+router.get('/:task_id', (req,res,next)=>{
+    Task.getTaskById(req.params.task_id)
+        .then(task => {
+            res.status(200).json(task);
+        })
+        .catch(next);
+});
+
+router.get('/', (req,res,next)=>{
+    Task.getTasks()
+        .then(tasks => {
+            res.json(tasks);
+        })
+        .catch(next);
+})
+
+
+router.post('/', (req,res,next) => {
+    Task.postTask(req.body)
+        .then(task => {
+            res.status(201).json(task);
+        })
+        .catch(next);
+})
+
+router.use((err,req,res,next) => {
+    res.status(500).json({
+        debugMessage: 'something went horribly wrong',
+        message:err.message,
+        stack:err.stack,
+    })
+})
 //export the module
 module.exports = router;
