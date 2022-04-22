67841a864748a3d6993c961a9c6988b0ace2a46c
billydean <billy.goehring@gmail.com>
2022-04-22 10:02:08 -0700

    is this starting to make sense? wow.

--- api/project/model.js
+++ api/project/model.js
@@ -2,6 +2,32 @@
 // gotta access that database somehow
 const db = require('../../data/dbConfig');
 
+/**
+ * Required Endpoints
+ * 
+ * [POST] "/"
+ * even though project_completed is stored as an integer, the API uses booleans when interacting with the client
+ * response body:
+ * {"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}
+ * 
+ * [GET] "/"
+ * [{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]
+ */
+
+function getProjectById(project_id) {
+    return db('projects')
+        .where('project_id', project_id)
+        .first();
+}
+
+function postProject(project) {
+   return db('projects').insert(project)
+    .then(([project_id]) => {
+        return db('projects')
+            .where('project_id', project_id)
+            .first()
+    })
+}
 
 //export the module
-module.exports = {}
+module.exports = {getProjectById, postProject}
--- api/project/router.js
+++ api/project/router.js
@@ -3,5 +3,42 @@
 const router = require('express').Router();
 const Project = require('./model');
 
+
+/**
+ * Required Endpoints
+ * 
+ * [POST] "/"
+ * even though project_completed is stored as an integer, the API uses booleans when interacting with the client
+ * response body:
+ * {"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}
+ * 
+ * [GET] "/"
+ * [{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]
+ */
+
+router.get('/:project_id', (req,res,next)=>{
+    Project.getProjectById(req.params.project_id)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(next);
+});
+
+router.post('/', (req,res,next) => {
+    Project.postProject(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(next);
+})
+
+router.use((err,req,res,next) => {
+    res.status(500).json({
+        debugMessage: 'something went horribly wrong',
+        message:err.message,
+        stack:err.stack,
+    })
+})
+
 //export the module
 module.exports = router;
